ad0c948662aa15c41333c45a448c3c3c7ad201e8
Pranadhx <pranadha101@gmail.com>
2025-02-10 22:39:32 +1100

    crop update

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,10 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://exlt.tech', 'https://www.exlt.tech', 'https://i.exlt.tech'],
+    origin: ['http://localhost:5173', 'https://exlt.tech'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Cross-Origin-Resource-Policy']
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Routes imports
